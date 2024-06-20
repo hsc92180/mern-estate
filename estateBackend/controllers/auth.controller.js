bf1d8d46d7932a8df80c6a2db4e7b14238d1eb41
hsc92180 <68774484+hsc92180@users.noreply.github.com>
2024-06-20 17:05:59 +0530

    create sign in api route

--- estateBackend/controllers/auth.controller.js
+++ estateBackend/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signup = async (req, res, next) => {
   console.log("Signup request received");
@@ -15,6 +19,35 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     console.log("Error creating user: ", error.message);
     // next(errorHandler(550, "error from the function signup in auth.controller.js"));
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  console.log("Signin request received");
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+    const vaidPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!vaidPassword) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
